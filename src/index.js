03bd937d3e9fbdf26d692762ef0c62630ec85876
Ryan Megidov <ryan@megidov.com>
2016-10-29 18:14:44 +0200

    Fixed routing and what not

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import createBrowserHistory from 'history/createBrowserHistory';
-import Component from 'inferno/dist/inferno-component';
-import Inferno from 'inferno/dist/inferno';
-import {
-  Router, 
-  Route
-} from 'inferno/dist/inferno-router';
-
+import Inferno from 'inferno';
+import { Router, Route } from 'inferno-router';
 import About from './About';
 import App from './App';
 import User from './User';
 
 const history = createBrowserHistory();
 
-Inferno.render((
+function renderDOM(location) {
+    Inferno.render((
     <Router history={ history }>
         <Route path="/" component={ App }>
-            <Route path="/" component={ About } />
-            <Route path="/:userId" component={ User } /> 
+            <Route path="/" component={ About }/>
+            <Route path="/:userId" component={ User }/>
         </Route>
     </Router>
-), document.getElementById("root"));
+    ), document.getElementById("root"));
+}
+
+// Render HTML on the browser
+renderDOM(history.location)
+history.listen(renderDOM)
+
+if (module.hot) {
+    module.hot.accept()
+}
